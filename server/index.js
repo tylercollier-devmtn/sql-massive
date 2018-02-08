f9419ecced664116b3ca252975dc3a0c9d009aad
Tyler Collier <tyler.collier+github@devmounta.in>
2018-02-08 11:46:41 -0700

    Add server and use massive

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const massive = require('massive');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+massive(process.env.CONNECTION_STRING).then(dbInstance => {
+  app.set('db', dbInstance);
+});
+
+app.get('/api/heroes', (req, res) => {
+  req.app.get('db').get_heroes().then(heroes => {
+    console.log('heroes', heroes)
+    res.status(200).json(heroes);
+  }).catch(error => {
+    console.log('Oh no! An error has happened!', error);
+    res.status(500).json({ message: 'An error happened' })
+  });
+})
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
